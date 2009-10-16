dd3e8af41037a1f57d007220e3ac79552d8c391c
scymtym <scymtym@694b31df-dcbb-44e8-af88-74c7ea918228>
2009-10-16 01:37:52 +0000

    * obby/rudel-obby-state.el (rudel-obby-state::connection): allow   objects of any type to be used as associated connection
    git-svn-id: https://svn.code.sf.net/p/rudel/code@445 694b31df-dcbb-44e8-af88-74c7ea918228

--- branches/future/obby/rudel-obby-state.el
+++ branches/future/obby/rudel-obby-state.el
@@ -51,7 +51,7 @@
 (defclass rudel-obby-state (rudel-impersonating-state)
   ((master-slot :initform 'connection)
    (connection  :initarg  :connection
-		:type     rudel-obby-socket-owner
+		:type     object
 		:documentation
 		"Connection object that uses the state."))
   "Base class for state classes used in the obby backend."
