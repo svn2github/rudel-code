6693912bdd64df595c59ff6b9097c5986f256a2c
scymtym <scymtym@694b31df-dcbb-44e8-af88-74c7ea918228>
2009-11-03 00:45:35 +0000

    * obby/rudel-obby-debug.el (header): downcased keywords; added version   0.2 to history   (rudel-obby-debug-old-state): removed; now handled by generic state   machine tracing functions   (rudel-obby-socket-owner::rudel-switch :before): removed; now   handled by generic state machine tracing functions   (rudel-obby-socket-owner::rudel-switch :after): removed; now handled   by generic state machine tracing functions   (rudel-obby-socket-owner::rudel-send): removed; now handled by   generic transport tracing functions   (rudel-obby-socket-owner::rudel-receive): removed; now handled by   generic transport tracing functions   (rudel-obby-socket-owner::rudel-message): removed; now handled by   generic transport tracing functions   (rudel-obby-connection::rudel-debug-target): new method; use debug   target of transport for connection
    git-svn-id: https://svn.code.sf.net/p/rudel/code@520 694b31df-dcbb-44e8-af88-74c7ea918228

--- branches/future/obby/rudel-obby-debug.el
+++ branches/future/obby/rudel-obby-debug.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2009 Jan Moringen
 ;;
 ;; Author: Jan Moringen <scymtym@users.sourceforge.net>
-;; Keywords: Rudel, obby, debugging
+;; Keywords: rudel, obby, debugging
 ;; X-RCS: $Id:$
 ;;
 ;; This program is free software: you can redistribute it and/or modify
@@ -27,96 +27,26 @@
 
 ;;; History:
 ;;
-;; 0.1 - Initial revision.
+;; 0.2 - New debug infrastructure
+;;
+;; 0.1 - Initial version
 
 
 ;;; Code:
 ;;
 
-(require 'eieio)
-
 (require 'rudel-debug)
 
-(require 'rudel-obby-util)
+(require 'rudel-obby-client)
 
 
-;;; Variables
+;;; Client connection debugging
 ;;
 
-(defvar rudel-obby-debug-old-state nil
-  "Saves state of state machines across one function call.")
-
-
-;;; Functions
-;;
-
-(defmethod rudel-send :before ((this rudel-obby-socket-owner)
-			       name &rest arguments)
-  "Print NAME and ARGUMENTS to debug stream."
-  (let ((message (apply #'rudel-obby-assemble-message
-			name arguments)))
-
-    (with-slots (socket) this
-      (rudel-debug-stream-insert
-       (rudel-debug-stream-name socket)
-       :sent
-       (concat  (substring message 0 (min (length message) 100))
-		(when (> (length message) 100)
-		  "..."))
-       (append (list name) arguments))))
-    )
-
-(defmethod rudel-receive :before ((this rudel-obby-socket-owner) data)
-  "Print DATA to debug stream."
-  (with-slots (socket) this
-    (rudel-debug-stream-insert
-     (rudel-debug-stream-name socket)
-     :received
-     (concat (substring data 0 (min (length data) 100))
-	     (when (> (length data) 100)
-	       "..."))))
-  )
-
-(defmethod rudel-message :before ((this rudel-obby-socket-owner)
-				  message)
-  "Print DATA to debug stream."
-  (let ((data (apply #'rudel-obby-assemble-message message)))
-
-    (with-slots (socket) this
-      (rudel-debug-stream-insert
-       (rudel-debug-stream-name socket)
-       :received-processed
-       (concat (substring data 0 (min (length data) 100))
-	       (when (> (length data) 100)
-		 "..."))
-       message)
-      ))
-  )
-
-(defmethod rudel-switch :before ((this rudel-obby-socket-owner)
-				 state &rest arguments)
-  "Store name of STATE for later printing."
-  (with-slots (state) this
-    (setq rudel-obby-debug-old-state
-	  (if state
-	      (object-name-string state)
-	    "#start")))
-  )
-
-(defmethod rudel-switch :after ((this rudel-obby-socket-owner)
-				state &rest arguments)
-  "Print STATE and ARGUMENTS to debug stream."
-  (with-slots (socket state) this
-    (let ((old-state rudel-obby-debug-old-state)
-	  (new-state (object-name-string state)))
-      (unless (string= old-state new-state)
-	(rudel-debug-stream-insert
-	 (rudel-debug-stream-name socket)
-	 :special
-	 (if arguments
-	     (format "%s -> %s %s" old-state new-state arguments)
-	   (format "%s -> %s" old-state new-state))))))
-  )
+(defmethod rudel-debug-target ((this rudel-obby-connection))
+  "Return debug target of the transport as debug target for THIS."
+  (with-slots (transport) this
+    (rudel-debug-target transport)))
 
 (provide 'rudel-obby-debug)
 ;;; rudel-obby-debug.el ends here
